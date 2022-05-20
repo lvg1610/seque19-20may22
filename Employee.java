f3e7141403535bc5d75b123dd0bd5f5f9cc83346
lvg1610 <lvg1610@gmail.com>
2022-05-20 15:03:50 +0530

    as v1 added Employee.java and Student.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee
+{
+    private int employeeId;
+    private String employeeName;
+    private double salary;
+    private String departmentName;
+
+    public Employee()
+    {
+    }
+ 
+    public String toString()
+    {
+    }
+}
---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    private int rollNo;
+    private String name;
+    private String[] subjects;
+    private LocalDate dateOfBirth;
+
+    public Student()
+    {
+    }
+
+    public boolean equals(Object obj)
+    {
+    } 
+}
