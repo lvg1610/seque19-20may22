8d283b927de2f37af078c9d2feb43879ba96478d
lvg1610 <64771079+lvg1610@users.noreply.github.com>
2022-05-20 15:54:12 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,8 @@
+public class Account
+{
+      private ing accountId;
+      private char accountType;
+      private double balanceAmount;
+      private LocalDate openedDate;
+      private char status;
+}
